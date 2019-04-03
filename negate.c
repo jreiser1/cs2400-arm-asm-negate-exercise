3ab1b3aa1af09fe7de28c6fa9e4f988ce0434154
Jacob Reiser <jreiser1@msudenver.edu>
2019-04-02 19:39:36 -0600

    Adjusting c program to resemble short negate

--- negate.c
+++ negate.c
@@ -1,16 +1,25 @@
-static const int ARRAY_SIZE = 5;
-
-int negate(int a) {
-    return (-1) * a;
-}
+int array[] = { 1, -2, 5, -6, 2 };
 
 int main() {
-    int array[] = { 1, -2, 5, -6, 2 };
 
-    for (int i = 0; i < ARRAY_SIZE; i ++) {
-        if (array[i] < 0) {
-            array[i] = negate(array[i]);
-        }
+    if(array[0] < 0) {
+        array[0] = 0 - array[0];
+    }
+
+    if(array[1] < 0) {
+        array[1] = 0 - array[1];
+    }
+
+    if(array[2] < 0) {
+        array[2] = 0 - array[2];
+    }
+
+    if(array[3] < 0) {
+        array[3] = 0 - array[3];
+    }
+    
+    if(array[4] < 0) {
+        array[4] = 0 - array[4];
     }
 
     return 0;
